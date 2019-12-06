36a120681332bc71e258467c43e74f62ae25a543
Mychal Estalilla <mychal.es@protonmail.com>
2019-12-05 19:20:00 -0800

    implement Login handler in Auth controller; start on Update and Delete user handlers

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,7 @@ const saltRounds = 10;
 
 module.exports = {
   createUser: (req, res, next) => {
-	const { username, password } = req.body;
+	const { username, password, email, phone } = req.body;
 
 	const createString = `
 	  INSERT INTO users (username, password)
@@ -25,6 +25,47 @@ module.exports = {
 		});
 	});
   },
+
+  loginUser: (req, res, next) => {
+	const { username, password } = req.body;
+
+	const queryString = `
+	  SELECT username, password FROM users
+	  WHERE username = $1
+	`
+
+	db.query(queryString, [username])
+	  .then( data => {
+		if (data.rows.length === 0) {
+		  return res.status(404).json({
+			invalid_auth: 'Invalid email or password'
+		  })
+		}
+		res.locals.email = data.rows[0].username;
+		const hashed_password = data.rows[0].password;
+
+		bcrypt.compare(password, hashed_password, (err, result) => {
+		  if (err) return next({err});
+		  if (result) {
+			return next();
+		  } else {
+			res.status(403).json({ invalid_auth: 'Invalid email or password' });
+		  }
+		});
+	  })
+	  .catch( err => {
+		if (env === 'dev') console.log('Error logging in user: ', err);
+		return next({ err });
+	  });
+
+  },
+
+  updateUser: (req, res, next) => {
+
+	const updateString = `
+	SELECT
+
+	`
 
 
 
