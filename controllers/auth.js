7a2a42c50b0968e72185349f3bdff1741707e5d9
Mychal Estalilla <mychal.es@protonmail.com>
2019-12-07 14:10:11 -0800

    implement user update functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,18 +5,18 @@ const saltRounds = 10;
 
 module.exports = {
   createUser: (req, res, next) => {
-	const { username, password, email, phone } = req.body;
+	const { email, password, name, phone } = req.body;
 
 	const createString = `
-	  INSERT INTO users (username, password)
-	  VALUES ($1, $2)
+	  INSERT INTO users (email, password, name, phone)
+	  VALUES ($1, $2, $3, $4)
 	`;
 
 	bcrypt.hash(password, saltRounds, (err, hash) => {
 	  if (err) return next({ err });
-	  db.query(createString, [username, hash])
+	  db.query(createString, [email, hash, name, phone])
 	    .then( data => {
-		  res.locals.username = username;
+		  res.locals.email = email;
 		  return next();
 		})
 		.catch( err => {
@@ -27,21 +27,22 @@ module.exports = {
   },
 
   loginUser: (req, res, next) => {
-	const { username, password } = req.body;
+	const { email, password } = req.body;
 
 	const queryString = `
-	  SELECT username, password FROM users
-	  WHERE username = $1
+	  SELECT email, password 
+	  FROM users
+	  WHERE email = $1
 	`
 
-	db.query(queryString, [username])
+	db.query(queryString, [email])
 	  .then( data => {
 		if (data.rows.length === 0) {
 		  return res.status(404).json({
 			invalid_auth: 'Invalid email or password'
 		  })
 		}
-		res.locals.email = data.rows[0].username;
+		res.locals.email = data.rows[0].email;
 		const hashed_password = data.rows[0].password;
 
 		bcrypt.compare(password, hashed_password, (err, result) => {
@@ -62,11 +63,79 @@ module.exports = {
 
   updateUser: (req, res, next) => {
 
-	const updateString = `
-	SELECT
+	// TODO: Change logic to get email from cookie instead of from req.body
+	const { name, phone, email } = req.body;
+	const error = {};
 
-	`
 
+	// Update both name && phone number
+	if (name && phone) {
+	  const updateUser = `
+	  UPDATE users
+	  SET 
+	    name = $2,
+		phone = $3
+	  WHERE email = $1
+	  RETURNING name, phone
+	  `
+
+	  db.query(updateUser, [email, name, phone])
+		.then( data => {
+		  if (data.rows.length === 0) {
+			error[namephone] = 'No name or phone found';
+			return next(error);
+		  }
+		  console.log(data.rows[0]);
+		  next();
+		})
+	    .catch( err => next(err) );
+	}
+
+	// Update only name
+	else if (name) {
+	  const updateName = `
+	  UPDATE users 
+	  SET name = $2
+	  WHERE email = $1
+	  RETURNING name
+	  `
+
+	  db.query(updateName, [email, name])
+		.then( data => {
+		  if (data.rows.length === 0) {
+			error[name] = 'No name result found'; 
+			return next(error);
+
+		  }
+		  console.log(data.rows[0]);
+		  next();
+		})
+	    .catch( err => next(err) );
+	}
+
+	// Update only phone 
+	else if (phone) {
+	  const updatePhone = `
+	  UPDATE users
+	  SET phone = $2
+	  WHERE email = $1
+	  RETURNING phone
+	  `
+	  db.query(updatePhone, [email, phone])
+	    .then( data => {
+		  if (data.rows.length === 0) {
+			error[phone] = 'No phone result found';
+		    return next(error);
+		  }
+		  console.log(data.rows[0]);
+		  next();
+		})
+		.catch( err => next(err) );
+	}
+
+  },
+
+  // TODO: Delete user
 
 
 };
