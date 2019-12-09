374933c3a744ee15a7c7bfd89e7e1f01b5a4242b
Mychal Estalilla <mychal.es@protonmail.com>
2019-12-08 22:32:33 -0800

    set up frontend file structure for React and bootstrap Root component

--- server.js
+++ server.js
@@ -7,18 +7,24 @@ const cookieParser = require('cookie-parser');
 // Constants
 const PORT = process.env.PORT || 3000;
 
+// View engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 // Global middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Serve static files
-app.use('/assets', express.static(path.resolve(__dirname, './client/public')));
+app.use('/public', express.static(path.resolve(__dirname, './client/public')));
 
 // Route handlers
+const index = require('./routes/index');
 const authRouter = require('./routes/auth');
 
 // Routes
+app.use('/', index);
 app.use('/auth', authRouter);
 
 // Bootstrap server
