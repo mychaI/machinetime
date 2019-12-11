6511a537dc06428d43d8e1a4819d34b32f76da22
Mychal Estalilla <mychal.es@protonmail.com>
2019-12-10 20:58:04 -0800

    start to implement client-side routing

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+// Components
+import Landing from './components/presentation/Landing';
+import Register from './components/containers/Register';
 
 const Root = () => {
   return (
 	<>
-	  <h1>MachineTime</h1>
+	  <Router>
+		<h1>Hello</h1>
+		<ul>
+		  <li>
+			<Link to='/'>Landing</Link>
+		  </li>
+		  <li>
+			<Link to='/register'>Register</Link>
+		  </li>
+		</ul>
+	 	<Route exact path='/' component={Landing} />
+		<Route exact path='/register' component={Register} />	
+		
+	  </Router>
 	</>
   )
 }
