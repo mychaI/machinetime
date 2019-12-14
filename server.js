14358fca5a2e15ec75e86eb34e7d682c1a324ff7
JoshuaManto <joshuamanto@yahoo.com>
2019-12-14 15:16:01 -0800

    Required and initialized passport module and passport config file

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
 
 // Constants
 const PORT = process.env.PORT || 3000;
 
 // View engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Passport JWT
+app.use(passport.initialize());
+require("./config/passport")(passport);
 
 // Global middleware
 app.use(bodyParser.json());
@@ -17,15 +22,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Serve static files
-app.use('/public', express.static(path.resolve(__dirname, './client/public')));
+app.use("/public", express.static(path.resolve(__dirname, "./client/public")));
 
 // Route handlers
-const index = require('./routes/index');
-const authRouter = require('./routes/auth');
+const index = require("./routes/index");
+const authRouter = require("./routes/auth");
 
 // Routes
-app.use('/', index);
-app.use('/auth', authRouter);
+app.use("/", index);
+app.use("/auth", authRouter);
 
 // Bootstrap server
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
