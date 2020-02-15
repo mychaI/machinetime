e5b47042ec78ad3fad340db6fe842281bf2dee28
volta tumbleweed <mychal.es@protonmail.com>
2020-02-14 21:50:38 -0800

    set up Profile component

--- client/src/components/containers/Profile.js
+++ client/src/components/containers/Profile.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Auth';
+import axios from 'axios';
+
+import { InputLabel, TextField, Button } from '@material-ui/core';
+import { Delete, Save } from '@material-ui/icons'
 
 const Profile = props => {
+
+  const authContext = useContext(AuthContext);
+
+
+  useEffect( () => {
+	console.log('auth', authContext);
+  }, []);
+
+
   return (
-    <>
+    <div className='container'>
 	  <h1>User Profile</h1>
-	</>
+	  <form>
+		<InputLabel id='first-name-input'>First Name</InputLabel>
+		<TextField label={authContext.user.firstName} className='input' fullWidth variant='outlined' name='first_name' type='text' />
+		<InputLabel id='last-name-input'>Last Name</InputLabel>
+		<TextField label={authContext.user.lastName} className='input' fullWidth variant='outlined' name='first_name' type='text' />
+		<InputLabel id='phone-input'>Phone Number</InputLabel>
+		<TextField label={authContext.user.phone} className='input' fullWidth variant='outlined' name='first_name' type='text' />
+		<Button id='update-user' className='auth-button' color='primary' variant='contained' startIcon={<Save />}>Update Profile</Button>
+	  </form>
+	  <h1>Current Reservations</h1>
+
+	  <h1>Past Reservations</h1>
+
+
+
+      <h1>Danger Zone</h1>
+	  <Button id='delete-user' className='auth-button' color='secondary' variant='contained' startIcon={<Delete />}>Delete Account</Button>
+	</div>
   )
 }
 
