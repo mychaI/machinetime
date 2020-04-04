d414a355b5d4861cb2314b3fab60bc3840502a80
volta tumbleweed <mychal.es@protonmail.com>
2020-04-03 23:57:28 -0700

    start scaffolding out landing page structure and styling

--- client/src/components/presentation/Landing.js
+++ client/src/components/presentation/Landing.js
@@ -4,7 +4,26 @@ const Landing = () => {
 
   return (
 	<>
-	  <h1>Welcome to MachineTime</h1>
+	  <div id='landing-page'>
+	    <div className='landing-bg'>
+		</div>
+		<section id='hero'>
+		  <h1>Welcome to MachineTime</h1>
+		</section>
+		<section id='details'>
+		  <h2> Details about MachineTime </h2>
+		  <figure className='detail-item'>
+		    <div className='image-frame'>
+
+			</div>
+			<div className='animated fadeInRight'>
+			</div>
+		  </figure>
+
+		  <figure className='detail-item'>
+		  </figure>
+		</section>
+	  </div>
 
 	</>
   )
